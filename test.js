2486cdf11b22376bb02589dba703b49021d2e1d8
Sijmen J. Mulder <ik@sjmulder.nl>
2018-07-29 22:51:57 +0200

    Remove execa dependency

--- test.js
+++ test.js
@@ -1,11 +1,7 @@
+import {execSync} from 'child_process';
 import test from 'ava';
-import execa from 'execa';
 
-test('main', async t => {
-	const {stdout} = await execa('./cli.js', ['My Website', 'https://sindresorhus.com'], {
-		env: {
-			FORCE_HYPERLINK: 1
-		}
-	});
-	t.is(stdout, '\u001B]8;;https://sindresorhus.com\u0007My Website\u001B]8;;\u0007');
+test('main', t => {
+	const output = execSync('./cli.js "My Website" "https://sindresorhus.com"');
+	t.is(output.toString(), '\u001B]8;;https://sindresorhus.com\u0007My Website\u001B]8;;\u0007\n');
 });
